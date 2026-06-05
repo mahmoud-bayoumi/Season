7cb0b42eceb684a60c2d05e9d9016a430939eebb
Mahmoud Mohamed <mahmoudbayoumi110@gmail.com>
2026-06-05 01:28:30 -0700

    inital commit

---
+++ Season/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Season
+//
+//  Created by Bayoumi on 05/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Season/SeasonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeasonApp.swift
+//  Season
+//
+//  Created by Bayoumi on 05/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SeasonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
